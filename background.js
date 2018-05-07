2a2028574e4da000f634055bfcce8410c0ff73a5
RMeurisse <robinmeurisse@hotmail.com>
2018-05-07 21:58:56 +0200

    Updated message exchange between webpage and extension with flash param

--- background.js
+++ background.js
@@ -1,24 +1,20 @@
 var uploader = require('./lib/flash');
 
-// no need to open this window at all if you don't want, I left this in for dev purposes.
-chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('window.html', {
-    'outerBounds': {
-      'width': 400,
-      'height': 200
-    }
-  });
-});
-
 // when the webpage sends a message and we receive it, pass on the info to the uploader and request a flash to the device.
 chrome.runtime.onConnectExternal.addListener(function(port) {
-  port.onMessage.addListener(function(msg) {
-    // call flash process
-    uploader.flash(msg.board, msg.file, function(error) {
-      // prepare the response object
-      var message = error ? {error: error.message} : {success: true};
-      // send back the status of the flash to the webpage so it knows when it's done/errored.
-      port.postMessage(message);
-    });
-  });
+	port.onMessage.addListener(function(msg) {
+		// Check type of message: if flash = true -> flash device
+		if(msg.flash == true) {    
+			// call flash process
+			uploader.flash(msg.board, msg.file, function(error) {
+			  // prepare the response object
+			  var message = error ? {error: error.message} : {success: true};
+			  // send back the status of the flash to the webpage so it knows when it's done/errored.
+			  port.postMessage(message);
+			});
+		} else {
+			// If flash == false, webpage is checking if extension is active
+			port.postMessage({active: true});
+		}
+	});
 });
