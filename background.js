73e3578285da7192446eb16cbb9a6bd00e2b8a85
RMeurisse <robinmeurisse@hotmail.com>
2018-05-08 00:00:18 +0200

    Updated extension so webpage is able to detect the extension

--- background.js
+++ background.js
@@ -3,18 +3,24 @@ var uploader = require('./lib/flash');
 // when the webpage sends a message and we receive it, pass on the info to the uploader and request a flash to the device.
 chrome.runtime.onConnectExternal.addListener(function(port) {
 	port.onMessage.addListener(function(msg) {
-		// Check type of message: if flash = true -> flash device
-		if(msg.flash == true) {    
-			// call flash process
-			uploader.flash(msg.board, msg.file, function(error) {
-			  // prepare the response object
-			  var message = error ? {error: error.message} : {success: true};
-			  // send back the status of the flash to the webpage so it knows when it's done/errored.
-			  port.postMessage(message);
-			});
-		} else {
-			// If flash == false, webpage is checking if extension is active
-			port.postMessage({active: true});
-		}
+		// call flash process
+		uploader.flash(msg.board, msg.file, function(error) {
+			// prepare the response object
+			var message = error ? {error: error.message} : {success: true};
+			// send back the status of the flash to the webpage so it knows when it's done/errored.
+			port.postMessage(message);
+		});
 	});
 });
+
+// Webpage sends normal message to check if extension is installed. Respond to this message.
+chrome.runtime.onMessageExternal.addListener(
+	(message, sender, sendResponse) => {
+		if (message == 'check') {
+			sendResponse({
+				type: 'success'
+			});
+			return true;
+		}
+	}
+);
